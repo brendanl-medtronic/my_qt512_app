e85dd45071c1761ad2630c7b167f0ef90a82599a
Brendan LeFebvre <brendan.c.lefebvre@medtronic.com>
2020-05-08 14:24:30 -0400

    demo QOperatingSystemVersion

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QQmlContext>
 #include <QQuickView>
 
+#include <QOperatingSystemVersion>
+
 #include "Calculator.h"
 
 int main(int argc, char** argv)
@@ -9,6 +11,11 @@ int main(int argc, char** argv)
   Calculator calc;
 
   QGuiApplication app(argc, argv);
+
+  auto current_os = QOperatingSystemVersion::current();
+  QString os_name = current_os.name();
+
+  qDebug() << "Current OS: " << os_name;
 
   QQuickView view;
   view.rootContext()->setContextProperty("calc", &calc);
